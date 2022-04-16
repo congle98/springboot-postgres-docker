a900d84374dc0e0f558c5f930e1fc791b4af4c0c
congle98 <lethanhcongcnhn@gmail.com>
2022-04-16 12:17:41 +0700

    oke

--- src/main/java/com/app/springboot/swagger/SwaggerConfig.java
+++ src/main/java/com/app/springboot/swagger/SwaggerConfig.java
@@ -15,28 +15,28 @@ import static javax.management.Query.or;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Predicate;
-
-@EnableSwagger2
-@Configuration
-public class SwaggerConfig {
-
-
-    @Bean
-    public Docket postsApi() {
-        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
-                .apiInfo(apiInfo()).select().paths(postPaths()).build();
-    }
-
-    private Predicate<String> postPaths() {
-        return or(regex("/*"), regex("/api/person.*"));
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("JavaInUse API")
-                .description("JavaInUse API reference for developers")
-                .termsOfServiceUrl("http://javainuse.com")
-                .contact("javainuse@gmail.com").license("JavaInUse License")
-                .licenseUrl("javainuse@gmail.com").version("1.0").build();
-    }
-}
+//import com.google.common.base.Predicate;
+//
+//@EnableSwagger2
+//@Configuration
+//public class SwaggerConfig {
+//
+//
+//    @Bean
+//    public Docket postsApi() {
+//        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+//                .apiInfo(apiInfo()).select().paths(postPaths()).build();
+//    }
+//
+//    private Predicate<String> postPaths() {
+//        return or(regex("/*"), regex("/api/person.*"));
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder().title("JavaInUse API")
+//                .description("JavaInUse API reference for developers")
+//                .termsOfServiceUrl("http://javainuse.com")
+//                .contact("javainuse@gmail.com").license("JavaInUse License")
+//                .licenseUrl("javainuse@gmail.com").version("1.0").build();
+//    }
+//}
