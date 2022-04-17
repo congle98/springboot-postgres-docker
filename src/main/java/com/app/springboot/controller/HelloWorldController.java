379332e4366fc5db09cf06231f6ecbe28dbfbd5b
congle98 <79907265+congle98@users.noreply.github.com>
2022-04-17 21:35:42 +0700

    Update HelloWorldController.java

--- src/main/java/com/app/springboot/controller/HelloWorldController.java
+++ src/main/java/com/app/springboot/controller/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
     @GetMapping
     public String helloWorld(){
-        return "Công đẹp trai hihu";
+        return "Công đẹp trai higbfgdfhu";
     }
 
     @Autowired
